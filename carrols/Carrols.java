b5d63724802e9afc5c770b393cb4502edc1c8cbd
karifl <lampi_88@hotmail.com>
2020-04-21 16:25:30 +0300

    Update Carrols.java

--- carrols/Carrols.java
+++ carrols/Carrols.java
@@ -1,85 +1,85 @@
 package carrols;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import builder.*;
 
 public class Carrols extends BurgerBuilder{
 
-	private List <Object> club;
-	private TopBun TopBun;
-	private BottomBun BottomBun;
-	private Patty Patty;
-	private MiddleLayerBun MiddleLayer;
-	private Sauce Sauce;
-	private Salad Salad;
-	private Cheese Cheese;
-	private Object topBun;
-//	private Object bottomBun= "";
-//	private Object middleLayer= "";
-//	private Object patty= "";
-//	private Object sauce= "";
-//	private Object salad= "";
-//	private Object cheese= "";
+	private ArrayList <String> club = new ArrayList(8);
+	private TopBun TopBun = new TopBun();
+	private BottomBun BottomBun= new BottomBun();
+	private Patty Patty = new Patty();
+	private MiddleLayerBun MiddleLayer = new MiddleLayerBun();
+	private Sauce Sauce = new Sauce();
+	private Salad Salad = new Salad();
+	private Cheese Cheese = new Cheese();
+	private String topBun = "";
+	private String bottomBun= "";
+	private String middleLayer= "";
+	private String patty= "";
+	private String sauce= "";
+	private String salad= "";
+	private String cheese= "";
 	@Override
 	
 	public Carrols setTopBun() {
 		// TODO Auto-generated method stub
-		
-		this.topBun = TopBun;
+		this.topBun = TopBun.getInfo();
 		return this;
 	}
 	@Override
 	public Carrols setBottomBun() {
 		// TODO Auto-generated method stub
-//		this.bottomBun = BottomBun.getInfo();
+		this.bottomBun = BottomBun.getInfo();
+		//System.out.println(bottomBun);
 		return this;
 	}
 	@Override
 	public Carrols setMiddleLayer() {
 		// TODO Auto-generated method stub
-//		this.middleLayer =MiddleLayer.getInfo();
+		this.middleLayer =MiddleLayer.getInfo();
 		return this;
 	}
 	@Override
 	public Carrols setCheese() {
 		// TODO Auto-generated method stub
-//		this.cheese =Cheese.getInfo();
+		this.cheese =Cheese.getInfo();
 		return this;
 	}
 	@Override
 	public Carrols setSauce() {
 		// TODO Auto-generated method stub
-//		this.sauce =Sauce.getInfo();
+		this.sauce =Sauce.getInfo();
 		return this;
 	}
 	public Carrols setPatty(){
-//		this.patty =Patty.getInfo();	
+		this.patty =Patty.getInfo();	
 		return this;	
 	}
 	
 	@Override
 	public Carrols setSalad() {
 		// TODO Auto-generated method stub
-//		this.salad = Salad.getInfo();
+		this.salad = Salad.getInfo();
 		return this;
 	}
 	public Carrols( ){ 
 		}
 	
 	
-	public List getBurger() {
-		System.out.println("over here " + topBun.toString());
-		System.out.println("over here ");
-		//Burger burger= new Burger(this.bottomBun,this.topBun, this.cheese, this.middleLayer, this.patty, this.salad, this.sauce);
-//		club.add(burger.getSalad());
-//		club.add(burger.getSauce());
-//		club.add(burger.getPatty());
-//		club.add(burger.getMiddleLayer());
-//		club.add(burger.getPatty());
-//		club.add(burger.getCheese());
-//		club.add(burger.getTopBun());
-//		System.out.println(burger.getTopBun());
+	public ArrayList <String> getBurger() {
+		
+	
+		Burger burger= new Burger(this.topBun, this.bottomBun,this.patty,  this.middleLayer , this.salad, this.sauce, this.cheese);
+		club.add(0,burger.getBottomBun());
+		club.add(1,burger.getSalad());
+		club.add(2,burger.getSauce());
+		club.add(3,burger.getPatty());
+		club.add(4,burger.getMiddleLayer());
+		club.add(5,burger.getPatty());
+		club.add(6,burger.getCheese());
+		club.add(7,burger.getTopBun());
 		return club;
 	}
 
